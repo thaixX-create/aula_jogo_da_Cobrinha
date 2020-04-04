0c8ddeb20be80c9022c5d30ddead371a0ffbee7f
thaixX-create <thais.vieira0907@gmail.com>
2020-04-04 15:19:32 -0300

    criar comidinha

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
+}
 
 function criarBG() {
     context.fillStyle= "lightgreen";
-    context.fillRect(0,0,16 * box, 16 * box);
+    context.fillRect(0, 0, 16 * box, 16 * box);
     }
 
     function criarCobrinha(){
@@ -20,13 +24,18 @@ function criarBG() {
           }
 }
 
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
 document.addEventListener('keydown', update);
 
 function update (event){
-    if(event.keycode == 37 && direction != "right") direction= "left";
-    if(event.keycode == 38 && direction != "down") direction= "up";
-    if(event.keycode == 39 && direction != "left") direction= "right";
-    if(event.keycode == 40 && direction != "up") direction= "down";
+    if(event.keyCode == 37 && direction != "right") direction= "left";
+    if(event.keyCode == 38 && direction != "down") direction= "up";
+    if(event.keyCode == 39 && direction != "left") direction= "right";
+    if(event.keyCode == 40 && direction != "up") direction= "down";
 }
 
 function iniciarJogo(){
@@ -37,6 +46,8 @@ if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
     criarBG();
     criarCobrinha();
+    drawFood();
+
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
