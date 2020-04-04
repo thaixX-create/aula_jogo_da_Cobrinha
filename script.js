0e7151a17f0bfef533de64ddc5fc26988cbb3298
thaixX-create <thais.vieira0907@gmail.com>
2020-04-04 15:40:09 -0300

    definir aleatoriedade da frutinha

--- script.js
+++ script.js
@@ -44,9 +44,9 @@ if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box;
 if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
 if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
 
-    criarBG();
-    criarCobrinha();
-    drawFood();
+    criarBG( );
+    criarCobrinha( );
+    drawFood( );
 
 
     let snakeX = snake[0].x;
@@ -57,11 +57,17 @@ if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box;
     if(direction == "up") snakeY -= box;
     if(direction == "down") snakeY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }
+    else{ food.x = Math.floor(Math.random() * 15 + 1) * box,
+          food.y = Math.floor(Math.random() * 15 + 1) * box
+
+    }
 
     let newHead = {
-        x: snake,
-        y: snake
+        x: snakeX,
+        y: snakeY
     }
 
     snake.unshift(newHead);
